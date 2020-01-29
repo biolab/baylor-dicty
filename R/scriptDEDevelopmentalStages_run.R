b0c963463cf0a1f3ca67ad3f35a82f02f93559e0
Hrovatin <karin.hrovatin@gmail.com>
2020-01-29 11:39:09 +0100

    run impulse on server

--- R/scriptDEDevelopmentalStages_run.R
+++ R/scriptDEDevelopmentalStages_run.R
@@ -22,9 +22,28 @@ strains=as.vector(unlist(unique(conditions$Strain),use.names=FALSE))
 mutant_list<-strains[strains!='AX4']
 
 #*************** Compare DE over whole profile with Impulse
+# Do it for mutant vs WT or between WT (TRUE=mutant vs WT)
+run_mutant_or_wt=FALSE
+if (run_mutant_or_wt){
+    # DE between mutant and WT
+    for( mutant in mutant_list){
+    print(mutant)
+    runImpulseCustomDispersion(conditions=conditions,genes=genes,times=NULL,case=mutant,control='AX4',main_lvl='Strain',
+               nested_dispersion=c('Time'),confounder_impulse=NULL,fdr=0.05,path=dataPathImpulse)
+    }
+}else{
+    # Compare DE between AX4 replicates to find where to use FDR cutoff: FD, PE, SE 1vs1
+    conditions_AX4=data.frame(conditions[conditions$Strain=='AX4',])
+    genes_AX4=genes[,rownames(conditions_AX4)]
+    conditions_AX4['Group']=unlist(lapply(conditions_AX4$Replicate,function(x) substring(x,1,6)),use.names = FALSE)
 
-for( mutant in mutant_list){
-  print(mutant)
-  runImpulseCustomDispersion(conditions=conditions,genes=genes,times=NULL,case=mutant,control='AX4',main_lvl='Strain',
-                           nested_dispersion=c('Time'),confounder_impulse=NULL,fdr=0.05,path=dataPathImpulse)
+    AX4_reps<-c('AX4_PE','AX4_SE','AX4_FD')
+    for (rep1 in 1:(length(AX4_reps)-1)){
+      for (rep2 in (rep1+1):length(AX4_reps)){
+        print(paste(AX4_reps[rep1],AX4_reps[rep2]))
+            runImpulseCustomDispersion(conditions=conditions_AX4,genes=genes_AX4,times=NULL,case=AX4_reps[rep1], 
+                                       control=AX4_reps[rep2], main_lvl='Group',nested_dispersion=c('Time'), 
+                                       confounder_impulse=NULL, fdr=0.05,path=dataPathImpulse)
+      }
+    }
 }
