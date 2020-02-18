af9cb8b773e402b2fe2816765e6504d8c4a2e943
Hrovatin <karin.hrovatin@gmail.com>
2020-02-18 10:44:16 +0100

    easy to use expression regulons heatmap code

--- networks/regulons_expression_heatmap.R
+++ networks/regulons_expression_heatmap.R
@@ -18,7 +18,7 @@ avg_expression=read.table(paste(path_regulons,"genes_averaged_orange_scale99perc
                           header=TRUE,row.names=1, sep="\t")
 #** Regulon groups tab file: First column lists genes (read in as rownames) and 
 #** a column named Cluster specifying cluster/regulon of each gene
-regulons=read.table(paste(path_clusters,"mergedGenes_min18_clusters_smaller.tab",sep=''),
+regulons=read.table(paste(path_clusters,"mergedGenes_min18_clusters_larger.tab",sep=''),
                     header=TRUE,row.names=1, sep="\t")
 
 # Get clusters - list unique and sort
@@ -47,7 +47,7 @@ ht_list=Heatmap(t(avg_expression['Group']),show_column_names = FALSE, height = u
 #Time annotation
 times=unique(avg_expression$Time)
 #** Time colours
-col_time = colorRamp2( c(min(times),max(times)),c( "white", "blue"))
+col_time = colorRamp2( c(min(times),max(times)),c( "white", "#440154FF"))
 ht_time=Heatmap(t(avg_expression['Time']), height = unit(top_annotation_height, "cm"),
                 cluster_columns=FALSE, show_column_names = FALSE,name='Time',col=col_time,
                 heatmap_legend_param = list( at = c(min(times),as.integer(mean(c(min(times),max(times)))),max(times)),
@@ -59,7 +59,7 @@ expressions<-within(avg_expression, rm('Time', 'Strain','Group'))
 min_expression<-min(expressions)
 max_expression<-max(expressions)
 #** Expression colours
-col = colorRamp2(c(min_expression,max_expression), c( "cornflowerblue", "khaki1"))
+col = colorRamp2(c(min_expression,mean(c(min_expression,max_expression)),max_expression), c( "#440154FF", "#1F968BFF",'#FDE725FF'))
 first=TRUE
 for (cluster in clusters){
   print(cluster)
