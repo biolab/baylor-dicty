c50734a18b1305ccd07e8490eea92858253cac97
Hrovatin <karin.hrovatin@gmail.com>
2020-03-25 16:58:39 +0100

    enrichment regulons

--- stages_DE/enrichment_library.py
+++ stages_DE/enrichment_library.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 from statsmodels.stats.multitest import multipletests
 import pandas as pd
+import numpy as np
 
 from orangecontrib.bioinformatics.ncbi.gene import GeneMatcher
 from orangecontrib.bioinformatics.geneset.__init__ import (list_all, load_gene_sets)
@@ -80,7 +81,7 @@ def GO_enrichment(entrez_ids: list, organism: int = ORGANISM, fdr=0.25, slims: b
 
 def gene_set_enrichment(query_EID: set, reference_EID: set, gene_set_names: list = None, organism=ORGANISM,
                         padj_threshold: float = None, output: str = None, go_slims: bool = False,
-                        gene_sets_ontology: dict = None):
+                        gene_sets_ontology: dict = None, set_sizes: tuple = (-np.inf, np.inf),min_overlap:int=None):
     """
     Calculate enrichment for specified gene set ontologies. Padj is performed on combined results of all ontologies.
     Prior to p value calculation removes gene sets that do not overlap with query.
@@ -92,8 +93,11 @@ def gene_set_enrichment(query_EID: set, reference_EID: set, gene_set_names: list
     :param output: By default returns DataSetData object, if 'name': a dict with gene set name as key and padj as value,
     'name_ontology': a dict with (gene set name, ontology) tuple as key and padj as value
     :param go_slims: For Go use only generic slims if gene_sets_ontology not given
-    :param gene_sets_ontology: Dict with keys ontology names and values gene sets. Use this instead of obtaining gene sets based
-    on gene_set_names, go_slims and organism
+    :param gene_sets_ontology: Dict with keys ontology names and values gene sets. Use this instead of obtaining gene
+    sets based on gene_set_names, go_slims and organism
+    :param set_sizes: Use only gene sets with size greater or equal to 1st element of set_sizes and smaller or equal to
+    2nd element of set_sizes, default is -inf,inf
+    :param min_overlap: After padj calculation filter out gene sets that have ovarlap with query below min_overlap
     :return: List of GeneSetData objects
     """
     if gene_sets_ontology is None and gene_set_names is None:
@@ -103,17 +107,21 @@ def gene_set_enrichment(query_EID: set, reference_EID: set, gene_set_names: list
         gene_sets_ontology = get_gene_sets(gene_set_names=gene_set_names, organism=organism, go_slims=go_slims)
     for gene_set_name, gene_sets in gene_sets_ontology.items():
         for gene_set in gene_sets:
-            intersect_query = len(gene_set.genes.intersection(query_EID))
-            if intersect_query > 0:
-                result = gene_set.set_enrichment(reference=reference_EID, query=query_EID)
-                data = GeneSetData(gene_set=gene_set, ontology=gene_set_name)
-                data.pval = result.p_value
-                data.in_query = intersect_query
-                enriched.append(data)
+            gene_set_size = len(gene_set.genes)
+            if set_sizes[0] <= gene_set_size <= set_sizes[1]:
+                intersect_query = len(gene_set.genes.intersection(query_EID))
+                if intersect_query > 0:
+                    result = gene_set.set_enrichment(reference=reference_EID, query=query_EID)
+                    data = GeneSetData(gene_set=gene_set, ontology=gene_set_name)
+                    data.pval = result.p_value
+                    data.in_query = intersect_query
+                    enriched.append(data)
     if len(enriched) > 0:
         compute_padj(enriched)
         if padj_threshold is not None:
             enriched = [data for data in enriched if data.padj <= padj_threshold]
+        if min_overlap is not None:
+            enriched = [data for data in enriched if data.in_query >= min_overlap]
         if output == 'name':
             enriched = dict([(data.gene_set.name, data.padj) for data in enriched])
         elif output == 'name_ontology':
@@ -121,13 +129,16 @@ def gene_set_enrichment(query_EID: set, reference_EID: set, gene_set_names: list
     return enriched
 
 
-def get_gene_sets(gene_set_names: list, organism: str = ORGANISM, go_slims: bool = False) -> dict:
+def get_gene_sets(gene_set_names: list, organism: str = ORGANISM, go_slims: bool = False,
+                  set_sizes: tuple = (-np.inf, np.inf)) -> dict:
     """
     Get all gene sets.
     :param gene_set_names: Names of ontologies for which to get gene sets (as returned by list_gene_sets)
     :param organism: organism id
     :param go_slims: If ontology type (first element from tuples retured by list_gene_sets) is GO then output
     only gene sets that are in 'goslim_generic'
+    :param set_sizes: Use only gene sets with size greater or equal to 1st element of set_sizes and smaller or equal to
+    2nd element of set_sizes, default is -inf,inf
     :return: Dict with key is ontology name and values are its GeneSet objects
     """
     gene_set_ontology = dict()
@@ -139,7 +150,9 @@ def get_gene_sets(gene_set_names: list, organism: str = ORGANISM, go_slims: bool
     for gene_set_name in gene_set_names:
         gene_sets = load_gene_sets(gene_set_name, str(organism))
         if go_slims and gene_set_name[0] == 'GO':
-            gene_sets = [gene_set for gene_set in gene_sets if gene_set.gs_id in slims]
+            gene_sets = [gene_set for gene_set in gene_sets if gene_set.gs_id in slims
+                         and set_sizes[0] <= len(gene_set.genes) <= set_sizes[1]
+                         ]
         gene_set_ontology[gene_set_name] = gene_sets
     return gene_set_ontology
 
